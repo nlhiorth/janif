09841fbfda76abcd25254d0e7e16fce32d32dab9
Nils Hiorth <nils@nls.me>
2017-05-09 10:42:41 +0200

    Janif Classic is operational

--- janif-react/src/components/Buttons/Banana.jsx
+++ janif-react/src/components/Buttons/Banana.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import banana from '../../../img/banana.svg';
+import used_banana from '../../../img/used-banana.svg';
 
 class Banana extends Component {
 
   render() {
     return (
-      <img className="square button" src = {this.props.used ? 'used-banana.svg' : 'banana.svg'} onClick={() => this.props.onClick()} ></img>
+      <img className="square button" src = {this.props.used ? used_banana : banana} onClick={() => this.props.onClick()} ></img>
     );
   }
 }
