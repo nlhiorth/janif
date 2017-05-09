c7e4851c4fb4ca10bc94804e64a7b3a94357c5b2
Nils Hiorth <nils@nls.me>
2017-05-09 11:58:13 +0200

    Beta shit

---
+++ janif-react/src/components/Views/Scoring.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Scoring.css';
+import ScoreCard from '../Common/ScoreCard.jsx';
+
+class Scoring extends Component {
+
+  render() {
+    return (
+      <div className="Scoring">
+        <ScoreCard />
+      </div>
+    );
+  }
+}
+
+export default Scoring;
