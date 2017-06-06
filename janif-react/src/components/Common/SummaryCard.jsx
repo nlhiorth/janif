74cd05132a8cfe3b7ac63d5cc1d5b1d061af264d
Nils Hiorth <nils@nls.me>
2017-06-06 18:18:42 +0200

    Summary almost done

--- janif-react/src/components/Common/SummaryCard.jsx
+++ janif-react/src/components/Common/SummaryCard.jsx
@@ -4,36 +4,15 @@ import './SummaryCard.css';
 class SummaryCard extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      curplayer: 0,
-      players: [
-        {
-          id: 0,
-          name: 'NILS',
-          score: '',
-          condition: 'normal'
-        },
-        {
-          id: 1,
-          name: 'CHRIS',
-          score: '',
-          condition: 'normal'
-        },
-        {
-          id: 2,
-          name: 'PINGIZ',
-          score: '',
-          condition: 'normal'
-        }
-      ],
-    }
   }
 
   render() {
     return (
       <div className="SummaryCard">
-
+        <div className="Name">{this.props.name}</div>
+        <div className="Background">
+          <div className="Score">{this.props.score}</div>
+        </div>
       </div>
     );
   }
