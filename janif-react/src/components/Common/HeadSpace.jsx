bcb5bc3b08cee92f037f7551e52555176906d2e3
Nils Hiorth <nils@nls.me>
2017-06-14 12:45:50 +0200

    Improved Main View and added Header

---
+++ janif-react/src/components/Common/HeadSpace.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './HeadSpace.css';
+
+class HeadSpace extends Component {
+
+  render() {
+    return (
+      <div className="HeadSpace"></div>
+    );
+  }
+}
+
+export default HeadSpace;
--- janif-react/src/components/ViewManager.jsx
+++ janif-react/src/components/ViewManager.jsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import Scoring from './Views/Scoring.jsx'
 import Summary from './Views/Summary.jsx'
 import Main from './Views/Main.jsx'
+import Header from './Common/Header.jsx';
+import HeadSpace from './Common/HeadSpace.jsx';
 
 class ViewManager extends Component {
 
   render() {
     return (
       <div className="ViewManager">
+        <Header />
+        <HeadSpace />
         {false && <Scoring />}
         {false && <Summary />}
         {true && <Main />}
