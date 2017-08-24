91dea2ac23e124f8168b9c197f79fbf0ee32fc0d
Nils Hiorth <nils@nls.me>
2017-08-24 16:41:58 +0200

    Implemented some Motion

--- janif-react/src/components/ViewManager.jsx
+++ janif-react/src/components/ViewManager.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {TransitionMotion, Motion, spring} from 'react-motion';
 
 import Header from './Common/Header';
 import HeadSpace from './Common/HeadSpace';
@@ -12,6 +13,7 @@ import SetupView from '../containers/SetupView';
 transitionName="wipe"
 transitionEnterTimeout={500}
 transitionLeaveTimeout={500}>
+ style={{transform: 'translate(' + this.props.x + 'vw, 0)'}}
 */
 
 class ViewManager extends Component {
@@ -20,10 +22,18 @@ class ViewManager extends Component {
       <div className="ViewManager">
         {this.props.header && <Header resetState={this.props.resetState}/>}
         {this.props.header && <HeadSpace />}
-          {(this.props.curview === 'setup') && <SetupView key="scoring" gotoView={this.props.gotoView} />}
-          {(this.props.curview === 'scoring') && <ScoringView key="scoring" gotoView={this.props.gotoView} />}
-          {(this.props.curview === 'summary') && <SummaryView key="summary" gotoView={this.props.gotoView} />}
-          {(this.props.curview === 'main') && <MainView key="main" gotoView={this.props.gotoView} />}
+        <Motion defaultStyle={{x: 100}} style={{x: spring(0)}}>
+          {interpolatingStyle => (this.props.curview === 'setup') && <SetupView key="setup" style={{transform: 'translate3d(' + interpolatingStyle.x + 'vw, 0, 0)'}} gotoView={this.props.gotoView} />}
+        </Motion>
+        <Motion defaultStyle={{x: 100}} style={{x: spring(0)}}>
+          {interpolatingStyle => (this.props.curview === 'scoring') && <ScoringView key="scoring" style={{transform: 'translate3d(' + interpolatingStyle.x + 'vw, 0, 0)'}} gotoView={this.props.gotoView} />}
+        </Motion>
+        <Motion defaultStyle={{x: 100}} style={{x: spring(0)}}>
+          {interpolatingStyle => (this.props.curview === 'summary') && <SummaryView key="summary" style={{transform: 'translate3d(' + interpolatingStyle.x + 'vw, 0, 0)'}} gotoView={this.props.gotoView} />}
+        </Motion>
+        <Motion defaultStyle={{x: 100}} style={{x: spring(0)}}>
+          {interpolatingStyle => (this.props.curview === 'main') && <MainView key="main" style={{transform: 'translate3d(' + interpolatingStyle.x + 'vw, 0, 0)'}} gotoView={this.props.gotoView} />}
+        </Motion>
       </div>
     );
   }
--- janif-react/src/components/Views/Scoring.jsx
+++ janif-react/src/components/Views/Scoring.jsx
@@ -5,7 +5,7 @@ import ScoreCard from '../Common/ScoreCard.jsx';
 class Scoring extends Component {
   render() {
     return (
-      <div className="Scoring">
+      <div className="Scoring" style={this.props.style}>
         <ScoreCard players={this.props.players} curplayer={this.props.players[this.props.scoring.index]} index={this.props.scoring.index} rounds={this.props.scoring.rounds} curround={this.props.scoring.rounds[this.props.scoring.index]} onLoss={this.props.onLoss} onWin={this.props.onWin} onJanif={this.props.onJanif} nextPlayer={this.props.nextPlayer} prevPlayer={this.props.prevPlayer} setPoints={this.props.setPoints}gotoView={this.props.gotoView}/>
       </div>
     );
