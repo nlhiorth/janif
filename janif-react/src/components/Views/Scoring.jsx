9d215539503485770d4bb4c52e33ac31e266b7fa
Nils Hiorth <nils@nls.me>
2017-08-28 13:58:48 +0200

    Removed style as a prop

--- janif-react/src/components/Views/Scoring.jsx
+++ janif-react/src/components/Views/Scoring.jsx
@@ -5,7 +5,7 @@ import ScoreCard from '../Common/ScoreCard.jsx';
 class Scoring extends Component {
   render() {
     return (
-      <div className="Scoring" style={this.props.style}>
+      <div className="Scoring">
         <ScoreCard players={this.props.players} curplayer={this.props.players[this.props.scoring.index]} index={this.props.scoring.index} rounds={this.props.scoring.rounds} curround={this.props.scoring.rounds[this.props.scoring.index]} onLoss={this.props.onLoss} onWin={this.props.onWin} onJanif={this.props.onJanif} nextPlayer={this.props.nextPlayer} prevPlayer={this.props.prevPlayer} setPoints={this.props.setPoints}gotoView={this.props.gotoView}/>
       </div>
     );
