807e7e85c1e5087aec4439ad5bd1d73475da7b7c
Nils Hiorth <nils@nls.me>
2017-08-29 13:22:16 +0200

    Added some confettios

--- janif-react/src/components/Common/PlayerCard.jsx
+++ janif-react/src/components/Common/PlayerCard.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import Confetti from 'react-dom-confetti';
+
 import './PlayerCard.css';
 import Banana from '../Buttons/Banana.jsx';
 import Bean from '../Buttons/Bean.jsx';
+
+const config = {
+  angle: 90,
+  spread: 165,
+  startVelocity: 27,
+  elementCount: 113,
+  decay: 0.87
+};
 
 class PlayerCard extends Component {
   render() {
@@ -16,6 +26,7 @@ class PlayerCard extends Component {
           </div>
           <div className="Buttons">
             <Banana fresh={this.props.player.banana} onClick={() => this.props.onBanana(this.props.player.id)}/>
+            <Confetti active={this.props.player.banana} config={config}/>
             <Bean fresh={this.props.player.bean} onClick={() => this.props.onBean(this.props.player.id)}/>
           </div>
         </div>
