f719c5ad2bc2ca49cac0eab3516701cbdaf1ab14
Nils Hiorth <nils@nls.me>
2017-09-01 12:06:51 +0200

    Scoring buttons use CSS backgrounds instead of img

--- janif-react/src/components/Buttons/Win.jsx
+++ janif-react/src/components/Buttons/Win.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import banana from '../../../img/banana.svg';
-import used_banana from '../../../img/used-banana.svg';
+import './Scoring.css'
 
 class Win extends Component {
 
   render() {
     return (
-      <img className="square button" src={this.props.condition === 'win' ? used_banana : banana} onClick={() => this.props.onClick()} alt="Win"></img>
+      <div className={this.props.condition === 'win' ? 'WinSelected' : 'Win'} onClick={() => this.props.onClick()}></div>
     );
   }
 }
