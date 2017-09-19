fa29f4c6a7d0b8f6e8080ef32ba1451bfa06c48d
Nils Hiorth <nils@nls.me>
2017-09-19 11:02:52 +0200

    Removed server.js

---
+++ janif-node/config.js
@@ -0,0 +1,8 @@
+export default {
+  "apiKey": "AIzaSyDu3d7I2pAEVz8Izas7OYQDaGkj5shKFyI",
+  "authDomain": "janif-backend.firebaseapp.com",
+  "databaseURL": "https://janif-backend.firebaseio.com",
+  "projectId": "janif-backend",
+  "storageBucket": "janif-backend.appspot.com",
+  "messagingSenderId": "114103878367",
+}
