5ee33adc3908a80da5e3135594b53984f3aaa831
Nils Hiorth <nils@nls.me>
2017-09-19 11:05:48 +0200

    Nuke it

--- janif-node/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "apiKey": "AIzaSyDu3d7I2pAEVz8Izas7OYQDaGkj5shKFyI",
-  "authDomain": "janif-backend.firebaseapp.com",
-  "databaseURL": "https://janif-backend.firebaseio.com",
-  "projectId": "janif-backend",
-  "storageBucket": "janif-backend.appspot.com",
-  "messagingSenderId": "114103878367",
-}
