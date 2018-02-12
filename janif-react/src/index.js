7bf622301871a181c0c05cf5d4f5c0b4c967fd83
Nils Hiorth <nils@nls.me>
2018-02-12 16:16:25 +0100

    Removed obsolete test code

--- janif-react/src/index.js
+++ janif-react/src/index.js
@@ -51,50 +51,6 @@ let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
 )
 */
-/*
-store.dispatch(addPlayer("Nils"));
-store.dispatch(addPlayer("Ingvild"));
-store.dispatch(addPlayer("Christoffer"));
-store.dispatch(addPlayer("Martine"));
-*/
-/*
-store.dispatch(addScore(0, 23));
-store.dispatch(addScore(1, 13));
-store.dispatch(addScore(2, 36));
-store.dispatch(addScore(3, 54));
-store.dispatch(useBanana(1));
-store.dispatch(useBean(2));
-*/
-/*
-store.dispatch(addScore(3, 15));
-store.dispatch(addScore(0, 27));
-store.dispatch(addScore(0, 0));
-store.dispatch(addScore(1, 41));
-store.dispatch(setPoints(0, 0));
-store.dispatch(setPoints(1, 13));
-store.dispatch(setPoints(2, 0));
-store.dispatch(setPoints(3, 25));
-store.dispatch(roundWin(0));
-store.dispatch(roundLoss(1));
-store.dispatch(roundJanif(2));
-store.dispatch(nextPlayer()); // 1
-store.dispatch(nextPlayer()); // 2
-store.dispatch(prevPlayer()); // curplayer = 1
-store.dispatch(prevPlayer()); // curplayer = 0
-store.dispatch(prevPlayer()); // curplayer = 0
-store.dispatch(nextPlayer());
-store.dispatch(nextPlayer());
-store.dispatch(nextPlayer()); // curplayer = 3
-store.dispatch(nextPlayer()); // curplayer = 3
-*/
-/*store.dispatch(gotoView("summary", true));
-store.dispatch(gotoView("scoring", false));
-store.dispatch(gotoView("main"));
-store.dispatch(gotoView("setup"));
-*/
-
-
-/*unsubscribe();*/
 
 ReactDOM.render(
   <Provider store={store}>
